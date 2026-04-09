fce961fff350b98412b7ebdcfd870575977649ee
ismail <125493050+Xframex@users.noreply.github.com>
2026-04-09 20:00:18 +0100

    testing api verbs

--- Ecommerce/src/main/java/com/ismail/Ecommerce/config/MyDataRestConfig.java
+++ Ecommerce/src/main/java/com/ismail/Ecommerce/config/MyDataRestConfig.java
@@ -1,4 +1,32 @@
 package com.ismail.Ecommerce.config;
 
-public class MyDataRestConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+     @Override
+     // This method is used to configure the REST repository settings, such as exposing entity IDs and disabling certain HTTP methods.
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+         HttpMethod[] unsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        // Disable HTTP methods for Product: PUT, POST and DELETE
+        config.getExposureConfiguration()
+                .forDomainType(com.ismail.Ecommerce.entity.Product.class)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+        // Disable HTTP methods for ProductCategory: PUT, POST and DELETE
+        config.getExposureConfiguration()
+                .forDomainType(com.ismail.Ecommerce.entity.ProductCategory.class)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+    }
+
+
 }
