a0a280ffb3d6d24752d5eb8ae4d9eeb619b895f5
ismail <125493050+Xframex@users.noreply.github.com>
2026-04-17 02:34:49 +0100

    findbycategory method adding

--- Ecommerce/src/main/java/com/ismail/Ecommerce/dao/ProductRepository.java
+++ Ecommerce/src/main/java/com/ismail/Ecommerce/dao/ProductRepository.java
@@ -1,10 +1,19 @@
 package com.ismail.Ecommerce.dao;
 
 import com.ismail.Ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 @CrossOrigin(origins = "http://localhost:4200") // Allow cross-origin requests from the Angular frontend
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    
+
+    Page<Product> findByCategoryId(@Param ("id") Long id, Pageable pageable);
+
+
 }
+
+
