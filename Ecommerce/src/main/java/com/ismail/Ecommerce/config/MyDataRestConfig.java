51d3ff38a3534d8faa641dc80644d36d23f93a45
ismail <125493050+Xframex@users.noreply.github.com>
2026-04-19 04:20:58 +0100

    update service for dynamic categories

--- Ecommerce/src/main/java/com/ismail/Ecommerce/config/MyDataRestConfig.java
+++ Ecommerce/src/main/java/com/ismail/Ecommerce/config/MyDataRestConfig.java
@@ -1,15 +1,30 @@
 package com.ismail.Ecommerce.config;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
-     @Override
+    private EntityManager entityManager;
+
+
+    @Autowired
+    public MyDataRestConfig(EntityManager theEntityManager) {
+        this.entityManager = theEntityManager;
+    }
+
+    @Override
      // This method is used to configure the REST repository settings, such as exposing entity IDs and disabling certain HTTP methods.
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
          HttpMethod[] unsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
@@ -25,6 +40,25 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(com.ismail.Ecommerce.entity.ProductCategory.class)
                 .withItemExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions))
                 .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+        // Expose entity IDs in the JSON response for the Product and ProductCategory entities
+        exposeIds(config);
+    }
+    private void exposeIds(RepositoryRestConfiguration config) {
+        // Expose entity IDs in the JSON response for the Product and ProductCategory entities
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+        // Create an array of the entity classes
+        List<Class<?>> entityClasses = new ArrayList<>();
+
+        // Add the entity classes to the list
+        for (EntityType tempEntityType : entities) {
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+
+        // expose the entity IDs for the array of entity/domain types
+        Class<?>[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
+
 
     }
 
