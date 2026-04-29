b74a26f46effcd77133c81da5e998d1bb97a4d20
ismail <125493050+Xframex@users.noreply.github.com>
2026-04-29 19:45:59 +0100

    add Country and State entities with relationships; implement findByNameContaining method in ProductRepository

--- Ecommerce/src/main/java/com/ismail/Ecommerce/entity/State.java
+++ Ecommerce/src/main/java/com/ismail/Ecommerce/entity/State.java
@@ -17,7 +17,7 @@ public class State {
     @Column(name = "name")
     private String name;
 
-    @Column(name = "country")
+
     @ManyToOne
     @JoinColumn(name = "country_id")
     private Country country;
