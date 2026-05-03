852ed77d68376e1968101132d13b6524f397938c
ismail <125493050+Xframex@users.noreply.github.com>
2026-05-03 01:37:40 +0100

    add CheckoutController; implement placeOrder method for processing purchases

--- Ecommerce/src/main/java/com/ismail/Ecommerce/controller/CheckoutController.java
+++ Ecommerce/src/main/java/com/ismail/Ecommerce/controller/CheckoutController.java
@@ -1,4 +1,34 @@
 package com.ismail.Ecommerce.controller;
 
+import com.ismail.Ecommerce.dto.Purchase;
+import com.ismail.Ecommerce.dto.PurchaseResponse;
+import com.ismail.Ecommerce.service.CheckoutService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/api/checkout")
 public class CheckoutController {
+    // This controller will handle checkout-related operations,
+    // such as processing payments and managing orders.
+    private final CheckoutService checkoutService;
+
+    // Constructor injection of the CheckoutService
+    @Autowired
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+
+        // Call the service layer to process the purchase and return a response
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+        // Return the response to the client
+        return purchaseResponse;
+
+
+    }
 }
